2e32b20da5ec905a83a9d9a188ef3ba7944cd799
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-08-22 15:24:27 -0400

    updating employee table to be able to edit

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1>Employees</h1>
 
     <employee-form @add:employee="addEmployee" />
-    <employee-table :employees="employees" />
+    <employee-table :employees="employees" @delete:employee="deleteEmployee" @edit:employee="editEmployee" />
   </div>
 </template>
 
@@ -45,6 +45,12 @@ export default {
       const newEmployee = { ...employee, id };
 
       this.employees = [...this.employees, newEmployee];
+    },
+    deleteEmployee(id) {
+      this.employees = this.employees.filter((employee) => employee.id !== id);
+    },
+    editEmployee(id, updatedEmployee) {
+      this.employees = this.employees.map((employee) => (employee.id === id ? updatedEmployee : employee));
     },
   },
 };
